1dd0ce653cc5103274d664b6a7b369fe393e28cf
dangthai121200 <dangthai121200@gmail.com>
2022-09-12 20:35:53 +0700

    first commit

---
+++ src/main/java/com/com/thai/dang/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.com.thai.dang;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringAllApplication.class);
+	}
+
+}
